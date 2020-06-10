1008021df2fcc1913d136997becaa5c242c5a29e
2LeoCode <leo.13suardi@outlook.com>
2020-06-10 18:33:57 +0200

    suicidolino

---
+++ get_next_line_bonus.h
@@ -0,0 +1,39 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line_bonus.h                              :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: lsuardi <lsuardi@student.42.fr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2020/06/04 15:35:48 by lsuardi           #+#    #+#             */
+/*   Updated: 2020/06/09 16:11:05 by lsuardi          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef GET_NEXT_LINE_BONUS_H
+# define GET_NEXT_LINE_BONUS_H
+# include <unistd.h>
+# include <stdlib.h>
+# ifndef BUFFER_SIZE
+#  define BUFFER_SIZE 16
+# endif
+# define MAX_FD 9999
+
+typedef struct		s_list
+{
+	char			buf[BUFFER_SIZE + 1];
+	char			*pos;
+	struct s_list	*next;
+}					t_list;
+typedef struct		s_buffer
+{
+	int				read_ret;
+	int				init;
+	t_list			*all;
+	t_list			*last;
+	t_list			*cur;
+}					t_buffer;
+int					get_next_line(int fd, char **line);
+void				*lst_chr(t_list *lst, int c);
+int					clear_buffer(t_buffer *buf);
+#endif
